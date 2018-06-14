fb98261396d49f499e9e6ccd6227e385d00bec02
kevin <kevinjobrien420@gmail.com>
2018-06-13 20:15:11 -0500

    196 App Number 18 - Part2 - Create a Puzzle game by using Arrays in Java // Coding

--- app/src/main/java/com/example/kevin/createapuzzlegamebyusingarraysinjava/Puzzle.java
+++ app/src/main/java/com/example/kevin/createapuzzlegamebyusingarraysinjava/Puzzle.java
@@ -1,8 +1,29 @@
 package com.example.kevin.createapuzzlegamebyusingarraysinjava;
 
+import java.security.SecureRandom;
+
 public class Puzzle {
 
 
     private CompanyAndDevice[] companyAndDevices;
     private int currentDeviceModel;
+    private static final int NUMBER_OF_MODELS = 60;
+    private static final SecureRandom secureRandomNumbers = new SecureRandom();
+
+    public Puzzle() {
+
+        String[]  companies = { "Apple", "Google", "Sony", "Samsung", "HTC"};
+        String[] devices = {"Xperia Z3", "iPhone 6", "Galaxy Note8", "Galaxy Note10",
+                            "iPod Touch", "HTC M9", "Xperia Z5", "iPod Nano", "Xperia M",
+                            "Galaxy S6", "Nexus5", "Nexus10"};
+        companyAndDevices = new CompanyAndDevice[NUMBER_OF_MODELS];
+        currentDeviceModel = 0;
+
+        for (int index = 0; index < companyAndDevices.length; index++){
+
+            companyAndDevices[index] = new CompanyAndDevice()
+
+        }
+
+    }
 }
